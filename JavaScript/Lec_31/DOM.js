c1f48d8ebc7b5f0b4ec30a26efdd5515291be38f
Yogesh Kumar <yogeshkharb77@gmail.com>
2026-08-30 06:50:44 +0000

    dom first lecture

---
+++ JavaScript/Lec_31/DOM.js
@@ -0,0 +1,20 @@
+// DOM stands for document object model.
+/*
+    DIAGRAM OF THE WEBPAGE WILL BE LIKE THIS ONE  (this diagram is Document Object Model);
+                                    
+                                Window
+                                  |
+                                Document
+                                  |
+                -----------------HTML------------------------
+                |                                           |
+          -----Head-----                                   Body
+          |            |                                    |      
+        Title        Meta                      -------Div(Attribute)--------
+          |            |                       |                           |
+        text Mode    Attribute           h1(Attribute)                     p
+                                               |                           |
+                                            Text Mode                  Text Mode
+
+
+*/
